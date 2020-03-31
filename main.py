889d43fb6a0ce8470d9b7981bfd2b4a20a61e73e
eightys3v3n <eightys3v3n@gmail.com>
2020-03-30 21:07:48 -0600

    Added location parameter to import_file

--- main.py
+++ main.py
@@ -26,29 +26,34 @@ def restructure(data):
 
     return n_data
 
+def set_location(data, loc):
+    for r in data:
+        r['location'] = loc
 
-def import_chunk(database, data, module):
+
+def import_chunk(database, data, module, location=None):
     """Given a chunk of data (csv lines) and a module this will execute the SQL to import that data."""
     data = parse_rows(data, module)
+    if location is not None:
+        set_location(data, "Canada, Alberta, Calgary")
     data = restructure(data)
     execute_many(database, module.IMPORT_MANY_SQL, data)
 
     
-def import_file(database, path, module, chunk_size):
+def import_file(database, path, module, location=None, chunk_size=1024):
     reader = csv_file(path, fields=module.FIELD_NAMES)
 
     for chunk in progressbar.progressbar(grouper(reader, chunk_size), max_value=line_count(path)/chunk_size):
-        import_chunk(database, chunk, module)
+        import_chunk(database, chunk, module, location=location)
                         
 
 def main():
     logging.basicConfig(level=logging.WARNING)
     
     database = init_db()
-    import_file(database, "weatherstats_calgary_daily.csv", import_daily, 1024)
-    import_file(database, "weatherstats_calgary_hourly.csv", import_hourly, 1024)
+    import_file(database, "weatherstats_calgary_daily.csv", import_daily, location="Calgary, AB, Canada")
+    import_file(database, "weatherstats_calgary_hourly.csv", import_hourly, location="Calgary, AB, Canada")
 
-    
 
 if __name__ == '__main__':
     main()
